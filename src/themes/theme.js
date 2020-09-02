2145154ebce7f64f4d84f2c1f449cc3211fa114c
Luca La Marca <luca@MBP-di-Mac.homenet.telecomitalia.it>
2020-09-02 12:53:18 +0200

    creazione file gatsby browser e gatsby ssr con inizializzazione themeProvider

---
+++ src/themes/theme.js
@@ -0,0 +1,11 @@
+export default { 
+    fonts : {
+        title : "Poppins, sans-serif",
+        body : "Roboto, sans-serif",
+    },
+    color :{
+        primary: "#2680EB",
+        title: "#1c1c1",
+
+    }
+}
